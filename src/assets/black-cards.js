13cba63a3a93692cae399a657ba2e194af1cbb5a
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2017-12-31 18:23:28 -0500

    Add rest of cards

--- src/assets/black-cards.js
+++ src/assets/black-cards.js
@@ -18,182 +18,89 @@ const blackCards = [
   // Favorite vacation All is fair in ________ and ___________ (pick 2)
   'What I\'m giving up for Lent:',
   'It\'s no use crying over _____________.',
-  'My utopia includes _______________'
-  // All ________ is good
-  // _________ (pick 1)
-  // __________ is surely one of the
-  // signs of the apocolypse.
-  // _____________ is part of my
-  // faith exploration journey.
-  // The early bird gets the
-  // ______________
-  // __________ is just another way
-  // to say I love you.
-  // Why does no one today
-  // recognize the importance of
-  // ____________?
-  // If a genie granted me one wish, I
-  // would abolish ______________.
-  // Be on the lookout for a new
-  // ______________
-  // We\'ve got nothing to lose but our
-  // __________.
-  // Some are born to __________;
-  // some aspire to _________ and
-  // others have ___________ thrust
-  // upon them (1 card)
-  // _________ is my hope of
-  // salvation.
-  // ___________: It\'s what real
-  // mennonites do.
-  // _________ always gets me into
-  // trouble.
-  // ____________ makes me want
-  // to _________. (pick 2)
-  // On earth as it is in Heaven just
-  // means ____________.
-  // My Church taught me to feel
-  // guilty about ___________.
-  // Beauty may be in the eye of the
-  // beholder but ________ is
-  // universally adored.
-  // Sure, there\'s no peace without
-  // justice, but there\'s also no
-  // __________ without
-  // __________. (pick 2)
-  // Luke, use the ____________.
-  // A Little Bit of __________ is a
-  // Dangerous Thing.
-  // The Enemy of My Enemy is My
-  // Excuse for ______________.
-  // Toto, I\'ve a Feeling we\'re not in
-  // ________ anymore.
-  // They may take our lives but
-  // they\'ll never take our
-  // __________.
-  // Friends don\'t let friends engage
-  // in ___________.
-  // Will you let me be your
-  // _________? Pray that I will have
-  // the grace to let you be my
-  // _______ too. (pick 1)
-  // Do not let yourself be led into
-  // temptation by the
-  // ____________.
-  // If you haven\'t noticed ________,
-  // you\'re just not paying attention.
-  // All that Glitters is not
-  // ____________.
-  // All\'s Fair in Love and
-  // __________.
-  // As useful as _________ at a
-  // potluck.
-  // Death by a thousand
-  // __________.
-  // The rain falls on the _______
-  // and the _________ alike. (pick 2)
-  // Out of the frying pan and into the
-  // __________.
-  // A good ________ is hard to find. The grass is always greener on
-  // the other side of the ________.
-  // One who lives by the _________
-  // dies by the ___________.
-  // The Seven ________ of Highly
-  // Effective Mennonites
-  // The Life-Changing Magic of
-  // ____________.
-  // What to expect when you\'re
-  // ___________.
-  // A journey of a thousand miles
-  // begins with _________.
-  // One person\'s trash is another\'s
-  // __________.
-  // Sticks and stones may break my
-  // bones but ________ will never
-  // hurt me.
-  // The best things in life are
-  // ____________.
-  // Time flies when you\'re
-  // ___________.
-  // Truth is stranger than
-  // ___________.
-  // Be the ________ that you want
-  // to see in the world.
-  // Walk softly and carry a big
-  // ___________.
-  // My parents are_____________. You and I are related by
-  // __________.
-  //
-  // I know you\'re a Mennonite by
-  // your ____________
-  // My religion doesn\'t allow this in
-  // my house
-  // When the going gets tough, the
-  // Mennonites start talking about
-  // _____________.
-  // Seeing this term makes me think
-  // there might be Mennonite terms I
-  // know nothing about.
-  // Now is the perfect time for
-  // ____________.
-  // All I really need to know I learned
-  // in ___________.
-  // They say the pen is mightier than
-  // the sword but I say the
-  // _________ is mightier than the
-  // _________. (pick 2)
-  // You can never have too many
-  // ______________.
-  // If you don\'t have anything nice to
-  // say, say it with (or while)
-  // __________.
-  // It\'s not over until ____________.
-  // Just please stop it already with
-  // all that talk about ___________.
-  // My pastor has convinced me of
-  // ____________.
-  // The Gospel writers weren\'t
-  // thinking of ________ when they
-  // wrote the Bible.
-  // I would never talk about
-  // _________ in front of a fellow
-  // Mennonite.
-  // ___________ is where the power
-  // lies.
-  // We may not all be theologians
-  // but I know that ______ is True.
-  // __________ looks normal but it\'s
-  // not.
-  // __________ is the opiate of the
-  // masses.
-  // You won\'t believe what I learned
-  // about ___________.
-  // I\'m old enough to remember
-  // when ________ wasn\'t just a
-  // Mennonite thing.
-  // You had me at _________.
-  // Trust me - I\'m a _________.
-  // My Oma always told me that life
-  // is like a __________.
-  // A _________ a day keeps the
-  // deacon away.
-  // We\'re ignoring the ________
-  // again .
-  // Good __________ make good
-  // neighbours.
-  // I\'ve never met a ________ I
-  // didn\'t like.
-  // Chicken Soup for the
-  // __________ Soul
-  // ___________ are the devil\'s
-  // workshop.
-  // You\'re preaching to the
-  // __________.
-  // The only thing we have to fear is
-  // ____________.
-  // ____________ matters today
-  // more than ever.
-  // Never feed the ____________.
+  'My utopia includes _______________.',
+  'All ________ is good',
+  '_________ (pick 1)',
+  '__________ is surely one of the signs of the apocolypse.',
+  '_____________ is part of my faith exploration journey.',
+  'The early bird gets the ______________',
+  '__________ is just another way to say I love you.',
+  'Why does no one today recognize the importance of  ____________?',
+  'If a genie granted me one wish, I would abolish ______________.',
+  'Be on the lookout for a new ______________.',
+  'We\'ve got nothing to lose but our __________.',
+  'Some are born to __________;  some aspire to _________ and  others have ___________ thrust  upon them (1 card)',
+  '_________ is my hope of  salvation.',
+  '___________: It\'s what real  mennonites do.',
+  '_________ always gets me into  trouble.',
+  // '____________ makes me want  to _________. (pick 2)',
+  'On earth as it is in Heaven just  means ____________.',
+  'My Church taught me to feel  guilty about ___________.',
+  'Beauty may be in the eye of the  beholder but ________ is  universally adored.',
+  // 'Sure, there\'s no peace without  justice, but there\'s also no  __________ without  __________. (pick 2)',
+  'Luke, use the ____________.',
+  'A Little Bit of __________ is a  Dangerous Thing.',
+  'The Enemy of My Enemy is My  Excuse for ______________.',
+  'Toto, I\'ve a Feeling we\'re not in  ________ anymore.',
+  'They may take our lives but  they\'ll never take our  __________.',
+  'Friends don\'t let friends engage  in ___________.',
+  // 'Will you let me be your _________? Pray that I will have the grace to let you be my _______ too. (pick 1)',
+  'Do not let yourself be led into temptation by the ____________.',
+  'If you haven\'t noticed ________, you\'re just not paying attention.',
+  'All that Glitters is not ____________.',
+  'All\'s Fair in Love and __________.',
+  'As useful as _________ at a potluck.',
+  'Death by a thousand __________.',
+  // 'The rain falls on the _______ and the _________ alike. (pick 2)',
+  'Out of the frying pan and into the __________.',
+  'A good ________ is hard to find. The grass is always greener on the other side of the ________.',
+  'One who lives by the _________ dies by the ___________.',
+  'The Seven ________ of Highly Effective Mennonites',
+  'The Life-Changing Magic of ____________.',
+  'What to expect when you\'re ___________.',
+  'A journey of a thousand miles begins with _________.',
+  'One person\'s trash is another\'s __________.',
+  'Sticks and stones may break my bones but ________ will never hurt me.',
+  'The best things in life are ____________.',
+  'Time flies when you\'re ___________.',
+  'Truth is stranger than ___________.',
+  'Be the ________ that you want to see in the world.',
+  'Walk softly and carry a big ___________.',
+  'My parents are_____________. You and I are related by __________.',
+'  ',
+  'I know you\'re a Mennonite by your ____________',
+  'My religion doesn\'t allow this in my house',
+  'When the going gets tough, the Mennonites start talking about _____________.',
+  'Seeing this term makes me think there might be Mennonite terms I know nothing about.',
+  'Now is the perfect time for ____________.',
+  'All I really need to know I learned in ___________.',
+  // 'They say the pen is mightier than the sword but I say the _________ is mightier than the _________. (pick 2)',
+  'You can never have too many ______________.',
+  'If you don\'t have anything nice to say, say it with (or while) __________.',
+  'It\'s not over until ____________.',
+  'Just please stop it already with all that talk about ___________.',
+  'My pastor has convinced me of ____________.',
+  'The Gospel writers weren\'t thinking of ________ when they wrote the Bible.',
+  'I would never talk about _________ in front of a fellow Mennonite.',
+  '___________ is where the power lies.',
+  'We may not all be theologians but I know that ______ is True.',
+  '__________ looks normal but it\'s not.',
+  '__________ is the opiate of the masses.',
+  'You won\'t believe what I learned about ___________.',
+  'I\'m old enough to remember when ________ wasn\'t just a Mennonite thing.',
+  'You had me at _________.',
+  'Trust me - I\'m a _________.',
+  'My Oma always told me that life is like a __________.',
+  'A _________ a day keeps the deacon away.',
+  'We\'re ignoring the ________ again .',
+  'Good __________ make good neighbours.',
+  'I\'ve never met a ________ I didn\'t like.',
+  'Chicken Soup for the __________ Soul',
+  '___________ are the devil\'s workshop.',
+  'You\'re preaching to the __________.',
+  'The only thing we have to fear is ____________.',
+  '____________ matters today more than ever.',
+  'Never feed the ____________.'
 
 ]
 const randomBlack = function () {
--- src/assets/white-cards.js
+++ src/assets/white-cards.js
@@ -110,110 +110,174 @@ const whiteCards = [
   'Deaconesses',
   'Steinbach',
   'Peace and Conflict Studies',
-  'Theological Diversity'
-// Lancaster County Worldly Behaviour
-// The Conference
-// formerly known as the
-// General Conference
-// Bluffton College Old Colony Old Mennonites
-// Fresno Quilting Bee Old Order
-// Kitchener-Waterloo Volleyball
-// Tournaments Amish
-// Living More with Less walk-a-mile Holdemann
-// Community Assembly Patriarchy
-// Russian Mennonites Steinbach Church Elders
-// Swiss the Priesthood of All
-// Believers John Howard Yoder
-// Plautdeutch The Great Trek Bergthal Mennonites
-// Pennsylvania Dutch Playing the Mennonite
-// Game
-// Conscientious
-// Objectors
-// Mexican Mennonites World Conference Leamington
-// The New Hymn Book AMBS Hygiene Kits
-// Sharing a Hymn Book The Ban Redemptive Violence
-// Journeying Together Church Discipline War Taxes
-// A Water Pistol Sex while Standing Barn Raisings
-// Alcohol Post-Modern Theology Facebook
-// Raffle Tickets Peace Activism Restorative Justice
-// Bolivia The Quiet in the Land The Mission Field
-// Peacemaking Bible College Eschatology
-// Hymnody Bible quizzing Exegesis
-// The Bulletin Handel\'s Messiah Platz
-// Summer Sausage Choir Practice Faith Exploration
-// Classes
-// The Sunday School
-// Picnic Head Covering Faspa
-// True Evangelical Faith Suspenders Freiwilliges
-// Fraktur Sunflower Seeds Migration Patterns
-// The Benediction Mennonite Hipsters Public Confessions
-// Germantown The Relief Sale
-// Someone arguing that
-// Mennonite is a religion
-// not an ethnicity
-// Anabaptist history Thrift Stores Mennonite Names
-// Non-ethnic
-// Mennonites Mixed Marriages Pig Butchering Parties
-// Campfire Songs Missions 4 part harmony
-// 1525 The Salvation of Yash
-// Siemens Ministries
-// Brandy Mennonite-Made
-// Furniture Dogma
-// Accountability Sunday School Daily Vacation Bible
-// School
-// Signing Petitions Reconciliation Structures of Power
-// Positions of Privilege Generosity The Salt of the Earth
-// Farming Going on a Retreat Faith Formation
-// Rhubarb Lay Leadership Purity
-// Rudy Wiebe Communal Loneliness Barbara Smucker
-// Servant Moral Leadership Praise Songs
-// Coffee Hour Soup Passing Judgement on
-// Others
-// Sprinking Dunking Pouring
-// Child Evangelism Late Adopters of
-// Technology Auctions
-// Mud Sales Mennonite Brethren Hypocrisy
-// Evana Network Freundschaft Protestant Work Ethic
-// Verenijke The Elephant in the
-// Room Fences
-// Justice Happiness Tying Blankets
-// Crokinole
-// Claiming Other
-// People\'s Heritage as
-// Our Own
-// Fellowship
-// A Non-Practicing
-// Ethnic Mennonite
-// The Being a Faithful
-// Church Process Pacifism
-// Foot Washing
-// Services
-// The Great
-// Commission A Brood of Vipers
-// Intentional
-// Communities Gardening A Listening Group
-// Working Groups Vision Task Forces
-// The Lectionary Grapes and Crackers
-// for the Children
-// Insisting on being
-// identified as neither
-// Catholic nor Protestant
-// Consumer Boycotts Memorizing Bible
-// Verses
-// The Johnnie
-// Appleseed Song
-// First Cousins Once
-// Removed
-// Small Group
-// Gatherings
-// Wondering whether
-// Square Dancing
-// counts as Dancing
-// Geez Magazine
-// The Canadian
-// Mennonite
-// Newsmagazine
-// The Credit Union
+  'Theological Diversity',
+  'Lancaster County',
+  'Worldly Behaviour',
+  'The Conference formerly known as the General Conference',
+  'Bluffton College ',
+  'Old Colony ',
+  'Old Mennonites',
+  'Fresno ',
+  'Quilting Bee ',
+  'Old Order',
+  'Kitchener-Waterloo ',
+  'Volleyball Tournaments',
+  'Amish',
+  'Living More with Less',
+  ' walk-a-mile ',
+  ' Holdemann',
+  'Community ',
+  'Assembly ',
+  'Patriarchy',
+  'Russian Mennonites ',
+  'Steinbach Church',
+  'Elders',
+  'Swiss ',
+  'the Priesthood of All Believers',
+  ' John Howard Yoder',
+  'Plautdeutch ',
+  'The Great Trek ',
+  'Bergthal Mennonites',
+  'Pennsylvania Dutch ',
+  'Playing the Mennonite Game',
+  'Conscientious Objectors',
+  'Mexican Mennonites',
+  ' World Conference ',
+  ' Leamington',
+  'The New Hymn Book',
+  ' AMBS ',
+  ' Hygiene Kits',
+  'Sharing a Hymn Book ',
+  'The Ban ',
+  'Redemptive Violence',
+  'Journeying Together',
+  ' Church Discipline',
+  '  War Taxes',
+  'A Water Pistol ',
+  'Sex while Standing ',
+  'Barn Raisings',
+  'Alcohol ',
+  'Post-Modern Theology ',
+  'Facebook',
+  'Raffle Tickets ',
+  'Peace Activism ',
+  'Restorative Justice',
+  'Bolivia ',
+  'The Quiet in the Land ',
+  'The Mission Field',
+  'Peacemaking ',
+  'Bible College ',
+  'Eschatology',
+  'Hymnody ',
+  'Bible quizzing ',
+  'Exegesis',
+  'The Bulletin ',
+  'Handel\'s Messiah ',
+  'Platz',
+  'Summer Sausage ',
+  'Choir Practice ',
+  'Faith Exploration Classes',
+  'The Sunday School Picnic ',
+  'Head Covering ',
+  'Faspa',
+  'True Evangelical Faith ',
+  'Suspenders ',
+  'Freiwilliges',
+  'Fraktur ',
+  'Sunflower Seeds',
+  ' Migration ',
+  ' Patterns',
+  'The Benediction ',
+  'Mennonite Hipsters ',
+  'Public Confessions',
+  'Germantown ',
+  'The Relief Sale ',
+  'Someone arguing that Mennonite is a religion not an ethnicity',
+  'Anabaptist history ',
+  'Thrift Stores ',
+  'Mennonite Names',
+  'Non-ethnic Mennonites ',
+  'Mixed Marriages ',
+  'Pig Butchering Parties',
+  'Campfire Songs ',
+  'Missions ',
+  '4 part harmony',
+  '1525 ',
+  'The Salvation of Yash Siemens ',
+  'Ministries',
+  'Brandy ',
+  'Mennonite-Made Furniture ',
+  'Dogma',
+  'Accountability ',
+  'Sunday School ',
+  'Daily Vacation Bible School',
+  'Signing Petitions ',
+  'Reconciliation ',
+  'Structures of Power',
+  'Positions of Privilege ',
+  'Generosity ',
+  'The Salt of the Earth',
+  'Farming ',
+  'Going on a Retreat ',
+  'Faith Formation',
+  'Rhubarb ',
+  'Lay Leadership ',
+  'Purity',
+  'Rudy Wiebe ',
+  'Communal Loneliness ',
+  'Barbara Smucker',
+  'Servant ',
+  'Moral Leadership ',
+  'Praise Songs',
+  'Coffee Hour ',
+  'Soup ',
+  'Passing Judgement on Others',
+  'Sprinking ',
+  'Dunking ',
+  'Pouring',
+  'Child Evangelism',
+  ' Late Adopters of Technology ',
+  ' Auctions',
+  'Mud Sales ',
+  'Mennonite Brethren ',
+  'Hypocrisy',
+  'Evana Network ',
+  'Freundschaft ',
+  'Protestant Work Ethic',
+  'Verenijke ',
+  'The Elephant in the Room ',
+  'Fences',
+  'Justice',
+  ' Happiness ',
+  ' Tying Blankets',
+  'Crokinole',
+  'Claiming Other People\'s Heritage as Our Own',
+  'Fellowship',
+  'A Non-Practicing Ethnic Mennonite',
+  'The Being a Faithful Church Process ',
+  'Pacifism',
+  'Foot Washing Services',
+  ' The Great Commission',
+  '  A Brood of Vipers',
+  'Intentional Communities ',
+  'Gardening',
+  ' A Listening Group',
+  'Working Groups ',
+  'Vision ',
+  'Task Forces',
+  'The Lectionary ',
+  'Grapes and Crackers for the Children',
+  'Insisting on being identified as neither Catholic nor Protestant',
+  'Consumer Boycotts ',
+  'Memorizing Bible Verses',
+  'The Johnnie Appleseed Song',
+  'First Cousins Once Removed',
+  'Small Group',
+  'Gatherings',
+  'Wondering whether Square Dancing counts as Dancing',
+  'Geez Magazine',
+  'The Canadian Mennonite Newsmagazine',
+  'The Credit Union'
 ]
 const randomWhite = function () {
   return whiteCards[Math.floor(Math.random() * whiteCards.length)]
